8419df7393a95ac8f282977bac6a52fae594cdb5
Nikita <prey122@gmail.com>
2022-06-29 17:09:32 +0300

    header#1

--- src/components/header-account/header-account.js
+++ src/components/header-account/header-account.js
@@ -1,11 +1,24 @@
-import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ProfileIcon, LogoutIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './header-account.module.css';
+import React from 'react';
 
 function HeaderAccount() {
+    const [indexSelect, setIndexSelect] = React.useState(null);
+
+    const [loginEnter, setloginEnter] = React.useState(1);
+   
+    function handelClickLink(e) {
+        if (e.target.classList.contains('text_color_inactive')) {
+            setIndexSelect(parseInt(e.target.id, 10));
+        } else setIndexSelect(null)
+    }
+
     return(
         <div className={styles.container}>
-            <ProfileIcon type="secondary" style={{marginRight: '8px'}}/>
-            <a href="#" className='text text_type_main-default text_color_inactive'>Личный кабинет</a>
+            <ProfileIcon type={loginEnter === indexSelect ? 'primary' : 'secondary'}/>
+            <a href="#" className={`text text_type_main-default ${loginEnter === indexSelect ? '' : 'text_color_inactive'}`} id='1' onClick={handelClickLink}>
+            Личный кабинет
+            </a>
         </div>
     )
 }
--- src/components/nav-header/nav-header.js
+++ src/components/nav-header/nav-header.js
@@ -3,35 +3,31 @@ import { BurgerIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-co
 import React from "react";
 
 function NavHeader() {
-    const [typeBurger, setTypeBurger] = React.useState('secondary');
-    const [typeListIcon, setTypeListIcon] = React.useState('secondary');
-    const firstLink = React.useRef(null);
-    const secondLink = React.useRef(null);
+    const [typeBurger, setTypeBurger] = React.useState(1);
+    const [typeListIcon, setTypeListIcon] = React.useState(2);
 
-    const optonsBurger = {
-        
-    }
-
-    function handelClickLink(set, type, e, link, setAnother) {
-        e.preventDefault();
-        type === 'secondary' ? type = 'primary' : type = 'secondary';  
-        setAnother('secondary');
-        set(type);
-        e.currentTarget.classList.toggle('text_color_inactive');
-        link.current.classList.add('text_color_inactive');
-    }
+    const [selectIndex, setSelectIndex] = React.useState(1);
+   
+    function handelClickLink(e) {
+        const id = parseInt(e.target.id, 10);
+        setSelectIndex(id);
+     }
     
 
     return (
         <nav className={styles.nav}>
             <ul className={styles.ul}>
-                <li className={styles.li}>
-                    <BurgerIcon type={typeBurger}/>
-                    <a className="text text_type_main-default text_color_inactive" ref={firstLink} onClick={(e) => {handelClickLink(setTypeBurger, typeBurger, e, secondLink, setTypeListIcon)}} href="#">Конструктор</a>
+                <li className={styles.li} key={1}>
+                    <BurgerIcon type={typeBurger === selectIndex ? 'primary' : 'secondary'}/>
+                    <a className={typeBurger === selectIndex ? 'text text_type_main-default' : 'text text_type_main-default text_color_inactive'}  id="1" onClick={handelClickLink} href="#">
+                        Конструктор
+                    </a>
                 </li>
-                <li className={styles.li}>
-                    <ListIcon type={typeListIcon}/>
-                    <a className="text text_type_main-default text_color_inactive" ref={secondLink} onClick={(e) => {handelClickLink(setTypeListIcon, typeListIcon, e, firstLink, setTypeBurger)}} href="#">Лента заказов</a>
+                <li className={styles.li} key={2}>
+                    <ListIcon type={typeListIcon === selectIndex ? 'primary' : 'secondary'}/>
+                    <a className={typeListIcon === selectIndex ? 'text text_type_main-default' : 'text text_type_main-default text_color_inactive'}  id="2" onClick={handelClickLink} href="#">
+                        Лента заказов
+                    </a>
                 </li>
             </ul>
         </nav>
