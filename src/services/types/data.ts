c71c30b1f38f950f15762c0232aed938212c2979
Nikita <prey122@gmail.com>
2022-11-14 01:13:28 +0300

    have done first directories

---
+++ src/services/types/data.ts
@@ -0,0 +1,34 @@
+export type TItems = {
+  readonly _id: string;
+  readonly name: string;
+  readonly type: string;
+  readonly proteins: number;
+  readonly fat: number;
+  readonly carbohydrates: number;
+  readonly calories: number;
+  readonly price: number;
+  readonly image: string;
+  readonly image_mobile: string;
+  readonly image_large: string;
+}
+
+export type TOrderDetails = {
+  success: boolean;
+  name: string;
+  order: {
+    ingredients: Array<TItems>;
+    _id: string;
+    owner: {
+      name: string;
+      email: string;
+      createdAt: string;
+      updatedAt: string;
+    };
+    status: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    number: number;
+    price: number;
+  }
+}
