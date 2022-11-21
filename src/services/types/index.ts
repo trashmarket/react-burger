3b22ab6df743f37982657c9f3e146c07e9f66957
Nikita <prey122@gmail.com>
2022-11-21 23:30:33 +0300

    with pc problem check coomit

--- src/services/types/index.ts
+++ src/services/types/index.ts
@@ -1,3 +1,6 @@
 import { ThunkAction } from 'redux-thunk';
 import { Action, ActionCreator } from 'redux';
 import { store } from '../store';
+import { TcartActions } from '../actions/cart';
+import { TPassWordActions } from '../actions/password';
+import { TPersonActions } from '../actions/person';
