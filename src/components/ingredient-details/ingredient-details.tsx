95b4b3ce1ec3b0eb1b3e3adba7e3913f463b2eb6
Nikita <prey122@gmail.com>
2022-11-26 02:12:41 +0300

    before del .js

---
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from "react";
+import styles from './ingredient-details.module.css';
+import { TItems } from '../../services/types/data';
+
+type TIngredientDetails = {
+  ingredient: TItems
+}
+
+const IngredientDetails: FC<TIngredientDetails> = ({ingredient}) => {
+  
+  return (
+    <div className={styles.container}>
+      <h2 className="text text_type_main-large">Детали ингредиента</h2>
+      <div className={styles.content}>
+        <img
+          src={`${ingredient.image}`}
+          alt={ingredient.name}
+          width={520}
+          height={240}
+        />
+        <p className="text text_type_main-medium">{ingredient.name}</p>
+        <ul className={styles.ul}>
+          <li className="text text_type_main-default">
+            <span>Калории,ккал</span>
+            {ingredient.calories}
+          </li>
+          <li className="text text_type_main-default">
+            <span>Белки, г</span>
+            {ingredient.proteins}
+          </li>
+          <li className="text text_type_main-default">
+            <span>Жиры, г</span>
+            {ingredient.fat}
+          </li>
+          <li className="text text_type_main-default">
+            <span>Углеводы, г</span>
+            {ingredient.carbohydrates}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default IngredientDetails;
