d86da2b44963d2d61df73d84b400bd22338d4ac3
Nikita <prey122@gmail.com>
2022-11-26 16:03:51 +0300

    before del ingredients-main.js

---
+++ src/components/feed-main/feed-main copy.js
@@ -0,0 +1,79 @@
+import styles from './feed-main.module.css';
+import { useSelector, useDispatch} from 'react-redux';
+import { 
+  selectOrders,
+  startWsConnectionAction,
+  closeWsAction
+ } from '../../services/actions/ws-action';
+import { WS_CONNECTION_START_ALL } from '../../services/constants'
+import { selectCart } from '../../services/actions/cart'
+import { 
+  useEffect
+ } from 'react';
+import  OrdersList  from '../orders-list/orders-list';
+import Modal from "../modal/modal";
+import { OrderFullCard } from '../order-full-card/order-full-card';
+import { useHistory } from 'react-router-dom';
+import { checkHistory } from '../../utils/utils'
+
+export default function FeedMain({setUseModalState, ingredient, onClose}) {
+  const { orders, total, totalToday }  = useSelector(selectOrders);
+  const { items } = useSelector(selectCart);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  useEffect(()=>{
+    checkHistory(history);
+  }, [])
+
+  useEffect(() => {
+    dispatch(startWsConnectionAction(WS_CONNECTION_START_ALL, '/all'))
+    return () => {
+      dispatch(closeWsAction())
+    }
+  }, [dispatch])
+  
+  return (
+    <main className={styles.main}>
+      <h1 className="text text_type_main-large mt-10">Лента заказов</h1>
+      <section className={styles.section}>
+        <div className={styles.ordersWrapper}>
+          <OrdersList orders={orders} itemsCart={items} setUseModalState={setUseModalState}/>
+        </div>
+        <div className={styles.generalInformation}>
+          <div className={styles.generalInformationWraperList}>
+            <div>
+              <h3 className="text text_type_main-medium">Готовы:</h3>
+              <ul>
+                {orders
+                  .filter(({ status }) => status === "done")
+                  .splice(0, 5)
+                  .map(({ number }) => (
+                    <li className={`${styles.generalInformationWraperItem} text text_type_digits-default`} key={number}>{number}</li>
+                  ))}
+              </ul>
+            </div>
+            <div>
+              <h3 className="text text_type_main-medium">В работе:</h3>
+              <ul>
+                {orders.filter(({ status }) => status !== "done").splice(0, 5).map(({ number }) => (
+                    <li className={`${styles.generalInformationWraperItem} text text_type_digits-default`} key={number}>{number ? number : 'not status'}</li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+            <h3 className="text text_type_main-medium">Выполнено за все время:</h3>
+            <p className={`${styles.totalPrice} text text_type_digits-large`}>
+              {total}
+            </p>
+            <h3 className="text text_type_main-medium">Выполнено за сегодня:</h3>
+            <p className={`${styles.totalPrice} text text_type_digits-large`}>{totalToday}</p>
+        </div>
+        {ingredient && (
+        <Modal onClose={onClose}>
+          <OrderFullCard ingredient={ingredient}/>
+        </Modal>
+        )}
+      </section>
+    </main>
+  );
+}
---
+++ src/components/ingredients-main/ingredients-main.tsx
@@ -0,0 +1,53 @@
+import { useSelector } from "../../services/hooks";
+import { useEffect, useState } from "react";
+import styles from "./ingredients-main.module.css";
+import { useParams } from "react-router-dom";
+import { selectCart } from "../../services/actions/cart";
+import { TItems } from "../../services/types/data";
+export default function IngredientsMain() {
+  const [item, setItem] = useState<TItems | boolean | undefined>(false);
+  const { items } = useSelector(selectCart);
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    setItem(
+      items.find((item) => {
+        item._id === id;
+      })
+    );
+  }, [items]);
+
+  return (
+    item !== true &&
+    item !== undefined &&
+    item !== false && (
+      <div className={styles.container}>
+        <h2 className={`text text_type_main-large ${styles.title}`}>
+          Детали ингредиента
+        </h2>
+        <div className={styles.content}>
+          <img src={`${item.image}`} alt={item.name} width={520} height={240} />
+          <p className="text text_type_main-medium">{item.name}</p>
+          <ul className={styles.ul}>
+            <li className="text text_type_main-default">
+              <span>Калории,ккал</span>
+              {item.calories}
+            </li>
+            <li className="text text_type_main-default">
+              <span>Белки, г</span>
+              {item.proteins}
+            </li>
+            <li className="text text_type_main-default">
+              <span>Жиры, г</span>
+              {item.fat}
+            </li>
+            <li className="text text_type_main-default">
+              <span>Углеводы, г</span>
+              {item.carbohydrates}
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  );
+}
